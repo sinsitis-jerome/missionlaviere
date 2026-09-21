a498e6a6c34d00aa562edd72c5a6e0ac3ddb12e3
sinsitis-jerome <sinsitis@gmail.com>
2026-09-21 09:57:54 +0200

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -59,7 +59,7 @@ export interface Task {
 export type NewTaskInput = Pick
   Task,
   'title' | 'description' | 'category' | 'priority' | 'assigneeId' | 'dueDate' | 'recurrence'
->;
+;
 
 export type ActivityType =
   | 'creation'
